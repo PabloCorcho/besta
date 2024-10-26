e3ba7e1478a92d20ed17e3cb7546de4b2b06152e
Pablo Corcho-Caballero <p.corcho.caballero@rug.nl>
2024-10-26 11:18:58 +0100

    add new module

--- src/hbsps/pipeline_modules/__init__.py
+++ src/hbsps/pipeline_modules/__init__.py
@@ -2,5 +2,7 @@ from .base_module import BaseModule
 from .kin_dust import KinDustModule
 from .sfh_spectra import SFHSpectraModule
 from .sfh_photometry import SFHPhotometryModule
+from .full_spectral_fit import FullSpectralFitModule
 
-__all__ = ["BaseModule", "KinDustModule", "SFHSpectraModule", "SFHPhotometryModule"]
+__all__ = ["BaseModule", "KinDustModule", "SFHSpectraModule", "SFHPhotometryModule",
+           "FullSpectralFitModule"]
