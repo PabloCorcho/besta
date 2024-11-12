6efdc3fef6d4c49f030237d8a9a1950fa7676875
Pablo Corcho-Caballero <p.corcho.caballero@rug.nl>
2024-11-12 16:06:01 +0100

    update logo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -106,7 +106,7 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-html_logo = "_static/besta_transparent.png"
+html_logo = "besta_transparent.png"
 html_theme_options = {
     'logo_only': True,
     'display_version': False,
