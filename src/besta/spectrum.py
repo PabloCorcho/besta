2935e0a4ab1a08f29fdbaa5c4dc8588019d5a70a
Pablo Corcho-Caballero <p.corcho.caballero@rug.nl>
2025-02-04 10:59:37 +0100

    add legendre poly

--- src/besta/spectrum.py
+++ src/besta/spectrum.py
@@ -8,7 +8,7 @@ import scipy
 from scipy import ndimage
 from scipy.special import legendre
 from astropy import constants
-
+from astropy import units as u
 
 def log_rebin(lam, spec, velscale=None, oversample=1, flux=False):
     """
@@ -205,7 +205,7 @@ def smoothSpectrumFast(spectrum, sigmaPixel):
     return smoothSpectrum
 
 
-def get_legendre_polynomial_array(wavelength, order, bounds=None):
+def get_legendre_polynomial_array(wavelength, order, bounds=None, scale=100):
     """
     Compute an array of Legendre polynomials evaluated at normalized wavelengths.
 
@@ -219,6 +219,10 @@ def get_legendre_polynomial_array(wavelength, order, bounds=None):
         A tuple specifying the minimum and maximum bounds for normalization
         (bounds[0], bounds[1]). If None, the normalization is based on the
         minimum and maximum of the `wavelength` array.
+    scale : float, optional
+        A maximum scale to probe by the polynomials. If provided, the set of
+        polynomial will comprise the range that is sensitive to scales smaller
+        the input value (i.e. lower order polynomials are not included).
 
     Returns
     -------
@@ -228,13 +232,35 @@ def get_legendre_polynomial_array(wavelength, order, bounds=None):
         The shape of the array is (order + 1, len(wavelength)).
     """
     if bounds == None:
-        norm_wl = (
-            2 * (wavelength - wavelength.min()) / (wavelength.max() - wavelength.min())
-            - 1
-        )
+        bounds = wavelength.min(), wavelength.max()
+    
+    norm_wl = 2 * (wavelength - bounds[0]) / (bounds[1] - bounds[0]) - 1
+
+    if scale is not None:
+        min_order = np.round((bounds[1].value - bounds[0].value) / scale, 0)
     else:
-        norm_wl = 2 * (wavelength - bounds[0]) / (bounds[1] - bounds[0]) - 1
+        min_order = 1
+
+    if isinstance(norm_wl, u.Quantity):
+        norm_wl = norm_wl.decompose().value
+    print("Pol order == ", np.arange(min_order, min_order + order + 1))
     legendre_arr = np.array(
-        [np.array(legendre(deg)(norm_wl)) for deg in np.arange(0, order + 1)]
+        [np.array(legendre(deg)(norm_wl)) for deg in [0, *np.arange(min_order, min_order + order)]]
     )
     return legendre_arr
+
+def legendre_decorator(make_observable_mthd):
+    """Include multiplicative Legendre polynomials during a fit."""
+    def wrapper(*args, **kwargs):
+        if "legendre_pol" in args[0].config:
+            legendre_pol = args[0].config["legendre_pol"]
+            # Get the coefficients from the input DataBlock
+            coeffs = np.array([1.0] + [args[1]["parameters", f"legendre_{ith}"] for ith in range(1, legendre_pol.shape[0])])
+            output = make_observable_mthd(*args, **kwargs)
+            if isinstance(output, tuple):
+                return output[0] * np.sum(legendre_pol * coeffs[:, np.newaxis], axis=0), output[1]
+            else:
+                return output * np.sum(legendre_pol * coeffs[:, np.newaxis], axis=0)
+        else:
+            return make_observable_mthd(*args, **kwargs)
+    return wrapper
