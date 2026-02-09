04e72c644ddc340d3101b65f2932a2844c013bdb
Pablo Corcho-Caballero <p.corcho.caballero@rug.nl>
2026-02-09 09:34:11 +0100

    remove deprecated module

--- src/besta/pipeline_modules/__init__.py
+++ src/besta/pipeline_modules/__init__.py
@@ -1,12 +1,10 @@
 from .base_module import BaseModule
-from .kin_dust import KinDustModule
 from .sfh_photometry import SFHPhotometryModule
 from .full_spectral_fit import FullSpectralFitModule
 from .galaxy_spectra import GalaxySpectraModule
 
 __all__ = [
     "BaseModule",
-    "KinDustModule",
     "SFHPhotometryModule",
     "FullSpectralFitModule",
     "GalaxySpectraModule"
