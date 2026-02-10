137ecb6d5239fd94ee24063875ad8b68f80993ed
Pablo Corcho-Caballero <p.corcho.caballero@rug.nl>
2026-02-10 12:04:40 +0100

    add new photometry module

--- src/besta/pipeline_modules/__init__.py
+++ src/besta/pipeline_modules/__init__.py
@@ -2,10 +2,12 @@ from .base_module import BaseModule
 from .sfh_photometry import SFHPhotometryModule
 from .full_spectral_fit import FullSpectralFitModule
 from .galaxy_spectra import GalaxySpectraModule
+from .galaxy_photometry import GalaxyPhotometryModule
 
 __all__ = [
     "BaseModule",
     "SFHPhotometryModule",
     "FullSpectralFitModule",
-    "GalaxySpectraModule"
+    "GalaxySpectraModule",
+    "GalaxyPhotometryModule"
 ]
