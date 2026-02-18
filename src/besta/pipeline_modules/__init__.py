000f30f82ed96d565c1d912b7955abe9928bca73
Pablo Corcho-Caballero <p.corcho.caballero@rug.nl>
2026-02-18 08:43:46 +0100

    update fitting modules

--- src/besta/pipeline_modules/__init__.py
+++ src/besta/pipeline_modules/__init__.py
@@ -1,15 +1,15 @@
 from .base_module import BaseModule
-from .sfh_photometry import SFHPhotometryModule
 from .full_spectral_fit import FullSpectralFitModule
 from .galaxy_spectra import GalaxySpectraModule
 from .galaxy_photometry import GalaxyPhotometryModule
 from .sfh_photometry_grid import SFHPhotometryGridModule
+from .sfh_photometry_emu import SFHPhotometryEmulatorModule
 
 __all__ = [
     "BaseModule",
-    "SFHPhotometryModule",
     "FullSpectralFitModule",
     "GalaxySpectraModule",
-    "GalaxyPhotometryModule"
+    "GalaxyPhotometryModule",
     "SFHPhotometryGridModule",
+    "SFHPhotometryEmulatorModule",
 ]
