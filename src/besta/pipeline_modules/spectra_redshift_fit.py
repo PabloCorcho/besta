d56f916cf55930a7745a4d3f4055632803406d7e
Pablo Corcho-Caballero <p.corcho.caballero@rug.nl>
2026-05-18 08:18:31 +0200

    add docs, normalise chi2, save z likelihood

--- src/besta/pipeline_modules/spectra_redshift_fit.py
+++ src/besta/pipeline_modules/spectra_redshift_fit.py
@@ -93,7 +93,9 @@ def compute_redshift_chi2_from_slices(
             residual = scale * f - t
             chi2 += w * residual * residual
 
-        z_chi2[i] = chi2
+        # Normalise chi2 by the number of good pixels
+        if len(good_idx) > 0:
+            z_chi2[i] = chi2 / len(good_idx)
 
     return z_chi2, z_scales
 
