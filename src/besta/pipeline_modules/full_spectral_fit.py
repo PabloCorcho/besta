6539b63a16ee9cb6513a8983d91db8b5f7a38452
Pablo Corcho-Caballero <p.corcho.caballero@rug.nl>
2026-06-05 16:50:59 +0200

    fix lsf redshift

--- src/besta/pipeline_modules/full_spectral_fit.py
+++ src/besta/pipeline_modules/full_spectral_fit.py
@@ -72,7 +72,7 @@ class FullSpectralFitModule(SpectraFitModule):
         # Apply dust extinction
         dust_model = self.config["extinction_law"]
         flux_model = dust_model.apply_extinction(
-            self.config["wavelength"], flux_model, a_v=block["dust_attenuation", "a_v"]
+            self.config["wavelength"], flux_model, a_v=block["dust.attenuation", "a_v"]
         ).value
 
         weights = self.config["weights"] * mask
@@ -92,6 +92,7 @@ class FullSpectralFitModule(SpectraFitModule):
         if not valid:
             # To track invalid samples users can set debug=T
             # logger.warning("Invalid sample")
+            logger.debug("Invalid sample: %s", block)
             block[section_names.likelihoods, self.like_name] = -1e20 * penalty
             block["extra", "stellar_mass"] = np.nan
             return 0
