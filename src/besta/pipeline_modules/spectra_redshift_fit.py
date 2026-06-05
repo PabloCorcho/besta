8c4faa0149861a3ba113c449df8f305de5757a57
Pablo Corcho-Caballero <p.corcho.caballero@rug.nl>
2026-06-05 16:06:08 +0200

    renormiles by effective number of pixels

--- src/besta/pipeline_modules/spectra_redshift_fit.py
+++ src/besta/pipeline_modules/spectra_redshift_fit.py
@@ -343,7 +343,7 @@ class SpectraRedshiftFitModule(SpectraFitModule):
                              flux_model[good_pixels],
                              self.config["ivar"][good_pixels] * weights[good_pixels])
         # To make it compatible with photometric likelihoods
-        like /= np.sum(good_pixels)
+        like /= np.sum(weights[good_pixels])
         # Final posterior for sampling
         block[section_names.likelihoods, self.like_name] = like
         return 0
