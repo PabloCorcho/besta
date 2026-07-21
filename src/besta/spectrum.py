528f7e37adbc3fdee1c16da3625714277606cf9b
pcorchoc <p.corcho.caballero@rug.nl>
2026-07-21 09:39:04 +0200

    remove redundant code

--- src/besta/spectrum.py
+++ src/besta/spectrum.py
@@ -1745,9 +1745,6 @@ def find_emission_lines(
     if to_rest_frame:
         output_table["center"] = output_table["center"] / (1 + redshift)
         output_table["sigma"] = output_table["sigma"] / (1 + redshift)
-        if lines is not None:
-            output_table["velocity"] = output_table["velocity"]
-            output_table["velocity_dispersion"] = output_table["velocity_dispersion"]
         output_table.meta["rest_frame"] = True
         output_table.meta["redshift"] = redshift
 
